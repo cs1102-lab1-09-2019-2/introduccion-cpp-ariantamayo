48d590ca9e45cbcd6636d91cd077f291923fef25
Arián Tamayo <54276418+ariantamayo@users.noreply.github.com>
2019-08-21 22:54:35 -0500

    Exercise 9 poo solved

--- ejercicio9.cpp
+++ ejercicio9.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
-using std::cout;
-
+using namespace std;
 int main() {
-
-    int i = 1;
-    while (i <= 10) {
-        if (   ) {
-            //imprimir numeros pares verticalmente
+    for (int i; i <=10; i++){
+        if (i % 2 == 0) {
+            cout << i << "\n";
         }
-
     }
-}
+        }
