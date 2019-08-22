5c7168b23794fd3f69ca107a4b7c60decb195a1d
Arián Tamayo <54276418+ariantamayo@users.noreply.github.com>
2019-08-21 22:18:06 -0500

    Update ejercicio6.cpp

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -1,13 +1,14 @@
 
-
+#include <iostream>
+#include <vector>
+using namespace std;
 int main() {
     vector<vector<int>> v{{1, 2}, {3, 4}, {5, 6}};
     for (auto a : v) {
         for (auto i : a) {
-            // imprimir i
+            cout << i << "\n";
         }
         // salto de linea
     }
 }
-
 
