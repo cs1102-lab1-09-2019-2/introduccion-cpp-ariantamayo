96a1315f82227f7108531587e1cda17bd74317e3
Arián Tamayo <54276418+ariantamayo@users.noreply.github.com>
2019-08-21 21:41:02 -0500

    Update exercise 1 POO resolved

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,9 +1,8 @@
-
-
+#include <iostream>
+using namespace std;
 int main() {
 
-    std::cout<<"UTEC es la mejor Universidad!!\n";
+    cout<<"UTEC es la mejor Universidad!!\n";
 
     return 0;
 }
-
