eeaf47248be4d1bc90717620b3e4703891401568
Arián Tamayo <54276418+ariantamayo@users.noreply.github.com>
2019-08-25 21:04:11 -0500

    Exercise 12 poo solved

--- ejercicio12.cpp
+++ ejercicio12.cpp
@@ -1,16 +1,17 @@
-/*
- * Solamente incluir los header files que se requiren para hacer funcionar el programa
- */
+#include<iostream>
+#include<vector>
+#include <bits/stdc++.h>
 
+using namespace std;
 int main()
 {
-    std::vector<int> v = { 5, 2, 1, 3, 4, 2, 2, 4, 5, 5 };
+    vector<int> v = { 5, 2, 1, 3, 4, 2, 2, 4, 5, 5 };
 
-    std::unordered_set<int> s(v.begin(), v.end());
+    unordered_set<int> s(v.begin(), v.end());
     v.assign(s.begin(), s.end());
 
     for (auto it = v.cbegin(); it != v.cend(); ++it)
-        std::cout << *it << ' ';
+        cout << *it << ' ';
 
     return 0;
 }
