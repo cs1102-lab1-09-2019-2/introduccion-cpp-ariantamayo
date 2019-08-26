c993e5d3f3d7da7e9a963e407dc88bd120161c3b
Arián Tamayo <54276418+ariantamayo@users.noreply.github.com>
2019-08-25 20:27:22 -0500

    Exercise 10 solved

--- ejercicio10.cpp
+++ ejercicio10.cpp
@@ -1,21 +1,22 @@
+#include <iostream>
 #include <string>
-using std::string;
+using namespace std;
 
 int main() {
-    int a = 5;
+    char a[]={'c','a','d','e','n','a'};
     //Declarar una cadena
     string b;
     //Inicializar la cadena b
-    b = "Esto es una cadena";
+    b = 'Esto es una cadena XD';
     cout<<a<<"\n";
     cout<<b<<"\n";
     cout<<b[0]<<"\n";
     cout<<b[4]<<"\n";
 
-    int numeros{4, 8, 9, 5, 6, 7};
+    int numeros[]{4, 8, 9, 5, 6, 7};
     cout<<numeros[0]<<"\n";
 
     float numero{5.16};
-    cout<<numero<<"\n"
+    cout<<numero<<"\n";
 
 }
